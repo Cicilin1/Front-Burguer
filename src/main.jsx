ef3ef4defe0023e62d0f8a271f88fd2e05a867e7
Cicilini <guilherme.cicilini@hotmail.com>
2024-07-22 21:56:48 -0300

    config:global-styles

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import { Home } from './pages/Login/index';
+import GlobalStyles from './styles/globalStyles';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Home />
+    <GlobalStyles />
   </React.StrictMode>,
 );
---
+++ src/styles/globalStyles.js
@@ -0,0 +1,17 @@
+import { createGlobalStyle } from 'styled-components';
+
+const globalStyles = createGlobalStyle`
+
+    *{
+        margin: 0px;
+        padding: 0px;
+        box-sizing: border-box;
+        outline: none;
+    }
+
+    button{
+        cursor: pointer;
+    }
+`;
+
+export default globalStyles;
